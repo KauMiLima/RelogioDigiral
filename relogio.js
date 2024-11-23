d009974f2890a32ec083fde91b5251bdbe0468d1
Kaua <freekaua@gmail.com>
2024-11-23 12:48:47 -0300

    primeiro commit

--- relogio.js
+++ relogio.js
@@ -1,19 +1,18 @@
 function atualizarRelogio() {
     const agora = new Date();
     
-    // Obter horas, minutos e segundos
+  
     const horas = String(agora.getHours()).padStart(2, '0');
     const minutos = String(agora.getMinutes()).padStart(2, '0');
     const segundos = String(agora.getSeconds()).padStart(2, '0');
 
-    // Atualizar o conteúdo HTML com as horas, minutos e segundos
+  
     document.getElementById('horas').textContent = horas;
     document.getElementById('minutos').textContent = minutos;
     document.getElementById('segundos').textContent = segundos;
 }
 
-// Chamar a função a cada segundo para atualizar o relógio
+
 setInterval(atualizarRelogio, 1000);
 
-// Chamar a função imediatamente para mostrar o relógio sem atraso
 atualizarRelogio();
